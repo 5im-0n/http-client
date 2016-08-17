956a47b1861acac843515a13798d9587844606a3
s2 <s2@31337.it>
2016-08-17 15:16:15 +0200

    add icons

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let win
 
 function createWindow () {
 	// Create the browser window.
-	win = new BrowserWindow({width: 1200, height: 820})
+	win = new BrowserWindow({width: 1200, height: 820, icon: __dirname + '/http-client.png'});
 	win.setMenu(null);
 
 	// and load the index.html of the app.
