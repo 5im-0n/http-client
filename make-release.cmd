906a091799a6ea7adbe77fc78286685e78f594ad
s2 <s2@31337.it>
2016-08-17 14:38:34 +0200

    make zipfiles

--- make-release.cmd
+++ make-release.cmd
@@ -2,3 +2,9 @@ cd /d %~dp0
 
 cmd.exe /c node_modules\.bin\electron-packager . http-client --platform=linux --arch=x64 --overwrite
 cmd.exe /c node_modules\.bin\electron-packager . http-client --platform=win32 --arch=x64 --overwrite
+
+zip -r http-client-linux-x64.zip http-client-linux-x64
+zip -r http-client-win32-x64.zip http-client-win32-x64
+
+rm -rf http-client-linux-x64
+rm -rf http-client-win32-x64
